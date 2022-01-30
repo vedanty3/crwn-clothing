4f13c7fde080340c2efc83a7e830e4e754edde3b
Vedant Yetekar <ursvedantyetekar@gmail.com>
2022-01-30 13:50:26 +0530

    integrated our application with firebase

--- src/App.js
+++ src/App.js
@@ -15,11 +15,10 @@ class App extends React.Component {
       currentUser: null,
     };
   }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
       this.setState({ currentUser: user });
     });
   }
@@ -31,7 +30,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
