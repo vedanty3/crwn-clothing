bce5df0003d217b6368904494537914d68350ded
Vedant Yetekar <ursvedantyetekar@gmail.com>
2022-02-04 17:12:28 +0530

    updated shop data to new data, updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import "./cart-icon.styles.scss";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
+
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
